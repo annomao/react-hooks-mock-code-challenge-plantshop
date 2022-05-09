dd2fd4ce293324adb7d31d66f97d4d18eb09d8ec
annomao <omaoannkerubo@gmail.com>
2022-05-09 08:54:19 +0300

    add function to mark plant as sold out

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({plant}) {
   const {name ,image ,price} = plant
+
+  let [soldOut ,setSoldOut] = useState(true)
+
+  function handleStockClick(){
+    setSoldOut(()=> soldOut = !soldOut)
+  }
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {soldOut ? (
+        <button className="primary" onClick={handleStockClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockClick}>Out of Stock</button>
       )}
     </li>
   );
